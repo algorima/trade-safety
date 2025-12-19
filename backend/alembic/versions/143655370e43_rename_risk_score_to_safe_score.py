92ce36b6eade91f06e8b3c1bbfdf9ced2eab3c14
pjh <pjhdye423@gmail.com>
2025-12-19 21:06:42 +0900

    migration: risk_score를 safe_score로 컬럼명 변경

---
+++ backend/alembic/versions/143655370e43_rename_risk_score_to_safe_score.py
@@ -0,0 +1,30 @@
+"""rename risk_score to safe_score
+
+Revision ID: 143655370e43
+Revises: 46ef2cade112
+Create Date: 2025-12-19 20:52:22.163220
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '143655370e43'
+down_revision: Union[str, None] = '46ef2cade112'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # Rename column risk_score to safe_score (preserves data)
+    with op.batch_alter_table('trade_safety_checks', schema=None) as batch_op:
+        batch_op.alter_column('risk_score', new_column_name='safe_score')
+
+
+def downgrade() -> None:
+    # Rename column safe_score back to risk_score
+    with op.batch_alter_table('trade_safety_checks', schema=None) as batch_op:
+        batch_op.alter_column('safe_score', new_column_name='risk_score')
